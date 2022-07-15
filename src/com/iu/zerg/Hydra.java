16d4cf4ea0801f782bd9506257310585df18b448
Gobu1 <gogkgngg@gmail.com>
2022-07-15 14:02:46 +0900

    7.15

---
+++ src/com/iu/zerg/Hydra.java
@@ -0,0 +1,14 @@
+package com.iu.zerg;
+import com.iu.unit.Unit;
+
+public class Hydra extends Unit
+{
+	public void move()
+	{
+		System.out.println("이동");
+	}
+	
+	
+	//hp, color, name
+	//움직이고 공겨
+}
